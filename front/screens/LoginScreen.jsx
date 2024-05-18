5e40e753861efd7e761bd3a92bf823f03569592e
Leah Deshar <leah.desar@gmail.com>
2024-05-18 22:33:47 +0545

    Spotify mobile app clone � 🎼

--- front/screens/LoginScreen.jsx
+++ front/screens/LoginScreen.jsx
@@ -1,11 +1,141 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Platform, Pressable, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient";
+import { AntDesign, Entypo, MaterialIcons } from "@expo/vector-icons";
 
 const LoginScreen = () => {
+  const authenticate = () => {
+    console.log("authenticate");
+  };
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
+    <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
+      <SafeAreaView>
+        <View style={{ height: 80 }} />
+        <Entypo
+          name="spotify"
+          style={{ textAlign: "center" }}
+          size={80}
+          color={"white"}
+        />
+
+        <Text
+          style={{
+            color: "white",
+            fontSize: Platform.OS === "android" ? 28 : 40,
+            fontWeight: "bold",
+            textAlign: "center",
+            marginTop: 40,
+          }}
+        >
+          Millions of Songs Free on Spotify
+        </Text>
+
+        <View style={{ height: 80 }} />
+        <Pressable
+          onPress={authenticate}
+          style={{
+            backgroundColor: "#1DB954",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text>
+            Sign In With {Platform.OS == "android" ? "Spotify " : "Spotify"}
+          </Text>
+        </Pressable>
+
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            marginVertical: 10,
+            borderWidth: 0.8,
+            borderColor: "#C0C0C0",
+          }}
+        >
+          <MaterialIcons name="phone-android" size={24} color={"white"} />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            Continue with phone number
+          </Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            marginVertical: 10,
+            borderWidth: 0.8,
+            borderColor: "#C0C0C0",
+          }}
+        >
+          <AntDesign name="google" size={24} color={"red"} />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            Continue with Google
+          </Text>
+        </Pressable>
+        <Pressable
+          style={{
+            backgroundColor: "#131624",
+            padding: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            width: 300,
+            borderRadius: 25,
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "row",
+            marginVertical: 10,
+            borderWidth: 0.8,
+            borderColor: "#C0C0C0",
+          }}
+        >
+          <Entypo name="facebook" size={24} color={"#1028ff"} />
+          <Text
+            style={{
+              fontWeight: "500",
+              color: "white",
+              textAlign: "center",
+              flex: 1,
+            }}
+          >
+            Sign In with facebook
+          </Text>
+        </Pressable>
+      </SafeAreaView>
+    </LinearGradient>
   );
 };
 
