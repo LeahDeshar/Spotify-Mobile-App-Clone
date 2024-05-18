c03031a57d519034e18df78d9b148fc7daf013da
Leah Deshar <leah.desar@gmail.com>
2024-05-18 21:41:33 +0545

    Spotify mobile app clone �ð§

--- front/StackNavigator.js
+++ front/StackNavigator.js
@@ -7,26 +7,44 @@ import { Entypo, Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
+import LoginScreen from "./screens/LoginScreen";
 
 const Tab = createBottomTabNavigator();
 
 function BottomTab() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {
+          backgroundColor: "rgba(0,0,0,0.5)",
+          position: "absolute",
+          left: 0,
+          right: 0,
+          bottom: 0,
+          shadowOpacity: 4,
+          shadowRadius: 4,
+          elevation: 4,
+          shadowOffset: {
+            width: 0,
+            height: -4,
+          },
+          borderTopWidth: 0,
+        },
+      }}
+    >
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
           tabBarLabel: "Home",
-          headerShadow: false,
-          tabBarLabelStyle: { color: "black" },
-          tabBarIcon: ({ focused }) => {
+          headerShown: false,
+          tabBarLabelStyle: { color: "white" },
+          tabBarIcon: ({ focused }) =>
             focused ? (
-              <Entypo name="home" size={24} color={"black"} />
+              <Entypo name="home" size={24} color={"white"} />
             ) : (
-              <AntDesign name="home" size={24} color={"black"} />
-            );
-          },
+              <AntDesign name="home" size={24} color={"white"} />
+            ),
         }}
       />
       <Tab.Screen
@@ -35,14 +53,13 @@ function BottomTab() {
         options={{
           tabBarLabel: "Profile",
           headerShown: false,
-          tabBarLabelStyle: { color: "black" },
-          tabBarIcon: ({ focused }) => {
+          tabBarLabelStyle: { color: "white" },
+          tabBarIcon: ({ focused }) =>
             focused ? (
-              <Ionicons name="person" size={24} color={"black"} />
+              <Ionicons name="person" size={24} color={"white"} />
             ) : (
-              <Ionicons name="person-outline" size={24} color={"black"} />
-            );
-          },
+              <Ionicons name="person-outline" size={24} color={"white"} />
+            ),
         }}
       />
     </Tab.Navigator>
@@ -54,6 +71,12 @@ const StackNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ headerShown: false }}
+        />
+
         <Stack.Screen
           name="Main"
           component={BottomTab}
